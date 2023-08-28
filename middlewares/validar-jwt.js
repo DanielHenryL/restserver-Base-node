6e32979c75cdcdcf4c552340aed34fe1b4f12624
DanielHenryL <lagunasdaniel001@gmail.com>
2023-08-28 13:27:35 -0500

    Crear modelo de categorias y archivo de barril

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -1,6 +1,6 @@
 const { response, request } = require("express");
 const jwt = require('jsonwebtoken');
-const Usuario = require("../models/usuario");
+const { Usuario } = require("../models");
 const validarJwt = async( req = request, res = response, next ) =>{
     const token = req.header('x-token');
     // validar que hay un token.

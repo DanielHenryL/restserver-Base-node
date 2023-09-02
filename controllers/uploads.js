8aae76bbf878a9a36cd0c2f2745b290c2ddcfdd3
DanielHenryL <lagunasdaniel001@gmail.com>
2023-09-02 10:59:54 -0500

    Ubicar y cambiar de nombre

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 const { request, response } = require("express");
 
 const cargarArchivo = ( req = request, res = response ) =>{
@@ -21,8 +22,9 @@ const cargarArchivo = ( req = request, res = response ) =>{
         })
     }
 
-    const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
-  
+    const nombreTemp = uuidv4() + '.' + extension;
+    const uploadPath = path.join(__dirname, '../uploads/', nombreTemp);
+    
     archivo.mv( uploadPath, (err) => {
       if ( err ) {
         console.log( err )
